a7c4fe3bd259368d174b5e7094e84ae9f83b1d45
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-10-15 03:25:07 +0000

    Fixed codestyle in tests

--- tests/base_model_test.py
+++ tests/base_model_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import unittest
 import sys
-sys.path.append('..')
 from models.base_model import BaseModel
 
 
@@ -10,13 +9,14 @@ class BaseModelTest(unittest.TestCase):
         print("\nRunning setUp method...")
         self.model1 = BaseModel()
         self.model2 = BaseModel()
-    
+
     def test_basemodel_instantiation(self):
         print("Running Test case")
         self.assertIsNot(self.model1, self.model2)
 
     def tearDown(self):
         print("Running tearDown method...")
+
 
 if __name__ == '__main__':
     unittest.main()
