adcb3c1a554cd83c8418fbdf14426d694f4c7352
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-10-18 02:28:16 +0000

    base_model.py and test_base_model.py fixes

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,6 +2,7 @@
 import unittest
 import sys
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
@@ -17,6 +18,7 @@ class TestBaseModel(unittest.TestCase):
         self_attr = obj.__dict__
         string_format = "[{}] ({}) {}".format(class_name, self_id, self_attr)
         self.assertEqual(obj.__str__(), string_format)
+        self.assertIs(type(string_format), str)
 
     def test_id_instantiation(self):
         """Test obj.id is not None, ""
@@ -25,22 +27,33 @@ class TestBaseModel(unittest.TestCase):
         val = [None, ""]
         self.assertNotIn(obj, val)
 
-    def test_id_type(self):
+    def test__init__(self):
         """Test obj.id is of type str
         """
         obj = BaseModel()
         self.assertIs(type(obj.id), str)
+        self.assertIs(type(obj.created_at), datetime)
 
     def test_save(self):
         """
         """
         obj = BaseModel()
         obj.save()
+        self.assertIs(type(obj.updated_at), str)
+        d_json = obj.to_dict()
+        self.assertIs(type(d_json['updated_at']), str)
 
     def test_to_dict(self):
         """
         """
-        pass
+        obj = BaseModel()
+        obj.updated_at = datetime.utcnow()
+        dict_json = obj.to_dict()
+        self.assertIs(type(dict_json), dict)
+        self.assertIs(type(dict_json['id']), str)
+        self.assertIs(type(dict_json['created_at']), datetime)
+        self.assertIs(type(dict_json['__class__']), str)
+        self.assertEqual(dict_json['__class__'], obj.__class__.__name__)
 
 
 if __name__ == '__main__':
