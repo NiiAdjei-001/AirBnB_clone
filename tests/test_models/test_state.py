cf03ce6bf85cc4f976a53b37f0ff1a9a92090afd
Draco <niiadjeiosae.technology@gmail.com>
2023-10-30 18:37:52 +0000

    Testing unittest of test_state model

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -6,11 +6,13 @@ from models.state import State
 class TestStateClass(unittest.TestCase):
     """Test State Class
     """
-    def test_name(self):
+    def test_State(self):
         """"""
         state = State()
         type(state).name = None
+        state.name = 'John'
         self.assertIsNone(type(state).name)
+        self.assertIsNot(state.name, type(state).name)
 
 
 if __name__ == '__main__':
